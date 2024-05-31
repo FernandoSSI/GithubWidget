c2c17fc6c4f308973e3a129b7fb695ad9cf3cc65
FernandoSSI <fernandosi2005@gmail.com>
2024-05-30 22:07:39 -0300

    Initial commit

---
+++ GithubWidget/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/electron-vite.animate.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Bem vindo ao widget do github</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
